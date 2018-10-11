14ec95bcadbe5a77ca84b15dfe5782112493eddb
Derek King <derekbking@gmail.com>
2018-10-10 20:05:32 -0500

    Added comments to question 33

--- src/Question33.java
+++ src/Question33.java
@@ -6,9 +6,20 @@ public class Question33
     {
         ArrayList<Integer> vals = new ArrayList<Integer>();
 
+        /*
+         * Redundant boxing of integers ArrayList<Integer> will accept primitive
+         * data types too.
+         */
         vals.add(new Integer(31));
         vals.add(new Integer(41));
         vals.add(new Integer((int) 59.1));
+
+        /*
+         * ArrayList#set takes (int index, E element) where E is type of
+         * ArrayList. So both these lines update index 1 of the ArrayList,
+         * however, the final values is 119 because that assignment of index 1
+         * comes last.
+         */
         vals.set(1, 2);
         vals.set(1, new Integer(119));
 
